a56ef2a78539739726a7d38ad96327d0fd058c99
Gareth <grogers@thoughtworks.com>
2013-08-29 12:04:31 +0100

    Scripts to prerender treemap

--- render-graphs.py
+++ render-graphs.py
@@ -1,7 +1,7 @@
 import fnmatch
 import os
+import json
 import subprocess
-import multiprocessing
 
 matched_files = []
 domain = "http://localhost:8080/"
@@ -10,18 +10,15 @@ look_in = [ os.path.join('output', 'department'),
             os.path.join('output', 'high-volume-services') ]
 
 
-def render_html(url_fragment):
-    print "rendering %s status: %s" % (url_fragment, subprocess.call(['phantomjs', 'un-js.js', url_fragment, 'html_out', domain]))
-
-
 if __name__ == "__main__":
     for path in look_in:
         for root, dirnames, filenames in os.walk(path):
             for filename in fnmatch.filter(filenames, '*.html'):
                 ignore, url_fragment = os.path.join(root, filename).replace('.html', '').split('/', 1)
                 matched_files.append(url_fragment)
-    
-    pool = multiprocessing.Pool(processes=4)
-    pool.map(render_html, matched_files)
-    pool.close()
-    pool.join()
+
+    with open('treemaps.json', 'w') as out:
+        json.dump(matched_files, out)
+
+    subprocess.call(['phantomjs', 'un-js.js', 'http://localhost:8080/', 'treemaps.json', 'output'])
+
